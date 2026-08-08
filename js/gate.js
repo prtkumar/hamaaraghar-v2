529e39cf0a2e02d726c83d89aeac5ea503058bb8
prtkumar <prabhatkumar286266@gmail.com>
2026-08-08 15:26:34 +0530

    Update gate.js

--- js/gate.js
+++ js/gate.js
@@ -1,12 +1,19 @@
 const gate = document.getElementById("gate");
+const entrance = document.getElementById("entrance");
 
-gate.addEventListener("click",()=>{
+gate.addEventListener("click", () => {
 
     gate.classList.add("open");
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
-        window.location.href="home.html";
+        entrance.classList.add("hide");
+
+        setTimeout(() => {
+
+            window.location.href = "home.html";
+
+        },1200);
 
     },1600);
 
