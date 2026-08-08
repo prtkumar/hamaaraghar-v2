aac92c139c99e7626e500752b82bda613f735fdf
prtkumar <prabhatkumar286266@gmail.com>
2026-08-08 19:30:09 +0530

    Update gate.js

--- js/gate.js
+++ js/gate.js
@@ -1,7 +1,21 @@
 const gate = document.getElementById("gate");
+const entrance = document.getElementById("entrance");
+const world = document.getElementById("world");
 
-gate.addEventListener("click",function(){
+gate.addEventListener("click", () => {
 
-gate.classList.add("open");
+    gate.classList.add("open");
+
+    setTimeout(() => {
+        entrance.style.opacity = "0";
+    }, 1700);
+
+    setTimeout(() => {
+        entrance.style.display = "none";
+    }, 2500);
+
+    setTimeout(() => {
+        world.classList.add("show");
+    }, 600);
 
 });
