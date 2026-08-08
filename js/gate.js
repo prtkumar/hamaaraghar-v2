64ac5a61f1de3c8fa6b65a36bb101b3c7d8d611d
prtkumar <prabhatkumar286266@gmail.com>
2026-08-08 18:44:21 +0530

    Update gate.js

--- js/gate.js
+++ js/gate.js
@@ -1,28 +1,5 @@
 const gate = document.getElementById("gate");
-const entrance = document.getElementById("entrance");
 
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-
-let opened = false;
-
-gate.addEventListener("click", openGate);
-gate.addEventListener("touchstart", openGate, { passive: true });
-
-function openGate(){
-
-    if(opened) return;
-    opened = true;
-
-    left.style.transform = "translateX(-105%)";
-    right.style.transform = "translateX(105%)";
-
-    setTimeout(()=>{
-        entrance.style.opacity = "0";
-    },1800);
-
-    setTimeout(()=>{
-        entrance.style.display = "none";
-    },2600);
-
-}
+gate.onclick = function () {
+    alert("Gate Clicked");
+};
